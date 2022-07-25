7fa0a8e48e6de298e8be1cccdbafa1c6fe1820cb
Xiaotian Tang <tbedse12@gmail.com>
2022-07-24 23:34:47 -0400

    v1.1

--- FIR/Core.v
+++ FIR/Core.v
@@ -1,38 +1,36 @@
 module Core(
-input clk, 
-input clk_fifo,
-input reset, 
-input [15:0] X, 
-input [15:0] B,
-input in_write_ctrlX,
-output signed [37:0] sum,
-output able2write_out
+input 		clk, 
+input 		clk_fifo,
+input 		rstn, 
+input [15:0] 	data_in, 
+input		in_write_ctrlX,
+output[37:0]	sum,
+output		able2write_out
 );
 
-reg in_read_ctrlX;
-wire out_is_fullX, out_is_emptyX, out_is_fullB, out_is_emptyB;
-wire signed [15:0] out_read_dataX;
-wire signed [15:0] out_read_dataB;
-reg  signed [37:0] sum_partial, sum_partial_buffer;
-reg  signed [37:0] sum_out;
-reg  signed [37:0] sum_out_buffer;
-wire signed [31:0] Y, Y_1, Y_2, Y_3,Y_4;
-reg  signed [31:0] Y_buffer;
+
+wire	out_is_fullX, out_is_emptyX, out_is_fullB, out_is_emptyB;
+wire [15:0] out_read_dataX;
+wire [15:0] out_read_dataB;
+wire [31:0] Y, Y_1, Y_2, Y_3,Y_4;
+wire [6:0] read_count;
+reg  [37:0] sum_partial, sum_partial_buffer;
+reg  [37:0] sum_out;
+reg  [37:0] sum_out_buffer;
+reg  [31:0] Y_buffer;
 reg  [15:0] register [63:0];
 reg  [15:0] Y_register [63:0];
-reg [6:0] sum_count, read_count_buffer_1, read_count_buffer_2, read_count_buffer_3, read_count_buffer_4;
-reg able2write;
-wire [6:0] read_count;
+reg  [5:0] sum_count, read_count_buffer_1, read_count_buffer_2;
+reg  [4:0] counter;
 reg  [15:0] out_read_dataX_1, out_read_dataX_2, out_read_dataX_3, out_read_dataX_4, out_read_dataX_buffer_1, out_read_dataX_buffer_2;
-reg enable_1, enable_2;
+reg	able2write;
+reg	in_read_ctrlX;
+reg	enable_1, enable_2;
 integer v, i, x;
-reg [4:0] counter;
-	assign able2write_out = able2write; 
-
 
 	fifo 
-fifo( .clk_r(clk), .clk_w(clk), .rstn(reset), .in_read_ctrl(in_read_ctrlX), 
-.in_write_ctrl(in_write_ctrlX), .in_write_data(X), .out_read_data(out_read_dataX), 
+fifo( .clk_r(clk), .clk_w(clk), .rstn(rstn), .in_read_ctrl(in_read_ctrlX), 
+.in_write_ctrl(in_write_ctrlX), .in_write_data(data_in), .out_read_data(out_read_dataX), 
 .full(out_is_fullX), .empty(out_is_emptyX), .read_count(read_count));
 
 	DA_table_1 
@@ -44,25 +42,16 @@ DA_table_3(.table_in_3(out_read_dataX_3), .table_out_3(Y_3));
 	DA_table_4 
 DA_table_4(.table_in_4(out_read_dataX_4), .table_out_4(Y_4));
 
+assign able2write_out = able2write;
+assign sum = sum_out;
 
-	assign sum = sum_out;
-
- 
-always@(posedge clk or negedge reset)begin
-	read_count_buffer_1 <= read_count[5:0];
-	read_count_buffer_2 <= read_count_buffer_1;
-	read_count_buffer_3 <= read_count_buffer_2;
-	read_count_buffer_4[5:0] <= read_count_buffer_3;
-	out_read_dataX_buffer_1 <= out_read_dataX;
-	out_read_dataX_buffer_2 <= out_read_dataX;
-	if(reset)begin
+always@(posedge clk or negedge rstn)begin
+	if(!rstn)begin
 		in_read_ctrlX 		<= 0;
  		sum_partial   		<= 0;
 		sum_count    		<= 0;
 		read_count_buffer_1     <= 0;
 		read_count_buffer_2     <= 0;
-		read_count_buffer_3     <= 0;
-		read_count_buffer_4     <= 0;	
 		Y_buffer       		<= 0;
 		out_read_dataX_buffer_1 <= 0;
 		out_read_dataX_buffer_2 <= 0;
@@ -74,10 +63,13 @@ always@(posedge clk or negedge reset)begin
 			register[i] 	<= 0;
 			Y_register[i]	<= 0;
 				   end
-
-
-
 	end
+	else begin 
+		read_count_buffer_1 <= read_count;
+		read_count_buffer_2 <= read_count_buffer_1;
+		out_read_dataX_buffer_1 <= out_read_dataX;
+		out_read_dataX_buffer_2 <= out_read_dataX;
+
 
 	//fifo logic
 	if(out_is_emptyX)begin // if empty, write till full, if full, read till empty
@@ -94,7 +86,7 @@ always@(posedge clk or negedge reset)begin
 	     end
 
 
-	if(read_count[5:0] != 0 || read_count_buffer_1 == 63)begin //create reg to transfer fifo data
+	if(read_count[5:0] != 0 || read_count_buffer_1 == 63)begin //create reg to store and transfer fifo data for DA 
 		register[read_count_buffer_1] <= out_read_dataX;
 		if(read_count_buffer_1 < 16)begin 
 			for(i=0; i<16; i=i+1)begin 
@@ -121,7 +113,7 @@ always@(posedge clk or negedge reset)begin
 
 
 		
-	if	(enable_1)begin  // input data into LUT
+	if(enable_1)begin  // input data to LUT
 			if(v < 16)begin
 				out_read_dataX_4 <= Y_register[v + 48];
 				out_read_dataX_3 <= 0;
@@ -152,7 +144,7 @@ always@(posedge clk or negedge reset)begin
 
 
 
-	if(read_count_buffer_2 > 15 && read_count_buffer_2 < 32) begin //accumulating LUT outputs
+	if(read_count_buffer_2 > 15 && read_count_buffer_2 < 32) begin //sum the partial results 
 			if(read_count_buffer_2 == 31)
 				sum_partial <= sum_partial - (Y_1 <<< (read_count_buffer_2-16));
 			else
@@ -182,14 +174,18 @@ always@(posedge clk or negedge reset)begin
 				x <= x + 1;
 					   end	
 				if (x == 16) begin 
-					sum_out <= sum_partial;
+					sum_out_buffer  <= sum_partial;
 					sum_partial 	<= 0;
 					enable_2	<= 0;
-					x		<= 0;		 end
+					x		<= 0;		 
+						end
 			end
-					
-			
+		end		
 end
+
+always@(posedge clk_fifo) begin 
+	sum_out <= sum_out_buffer;
+	end
 		
 endmodule		
 				
--- FIR/asyn_fifo.v
+++ FIR/asyn_fifo.v
@@ -1,28 +1,28 @@
-module fifo(clk_r, clk_w, rstn, in_read_ctrl, in_write_ctrl, out_read_data, in_write_data, full, empty, gray_r_addr, gray_w_addr, read_count);
+module fifo(clk_r, clk_w, rstn, in_read_ctrl, in_write_ctrl, out_read_data, in_write_data, full, empty, read_count);
 
 parameter mem_depth = 63;
 parameter databits  = 16;
 parameter depth = 6; //addr bits
 
-input clk_r, clk_w, rstn, in_read_ctrl, in_write_ctrl;
-input  [databits-1:0] in_write_data;
+input	clk_r, clk_w, rstn, in_read_ctrl, in_write_ctrl;
+input	[databits-1:0] in_write_data;
 output wire full, empty;
 output reg [databits-1:0] out_read_data;
-
+wire [depth:0] gray_r_addr, gray_w_addr;
 reg [databits-1:0] mem [mem_depth:0];
 reg [depth:0] r_addr, w_addr, w2r_1, w2r_2, r2w_1, r2w_2;//extend 1 bits
-output wire [depth:0] gray_r_addr, gray_w_addr, read_count;   
+output wire [depth-1:0] read_count;   
 
 
 assign gray_w_addr = (w_addr >> 1) ^ w_addr;
 assign gray_r_addr = (r_addr >> 1) ^ r_addr;
 assign empty = (gray_r_addr == w2r_2);
 assign full = (gray_w_addr == {~r2w_2[6:5],r2w_2[4:0]});
-assign read_count = r_addr;
+assign read_count = r_addr[depth-1:0];
 	
 
 always@(posedge clk_r or negedge rstn)begin 
-	if (rstn)begin 
+	if (!rstn)begin 
 		r_addr <= 0;
 		w2r_1 <= 0;
 		w2r_2 <= 0;
@@ -40,7 +40,7 @@ always@(posedge clk_r or negedge rstn)begin
 end 
 
 always@(posedge clk_w or negedge rstn)begin 
-	if (rstn)begin 
+	if (!rstn)begin 
 		w_addr <= 0;
 		r2w_1 <= 0;
 		r2w_2 <= 0;
